a9b921d96af50114a7d79afb9c9ff14b68482356
Aamir Cheema <amircheema295@gmail.com>
2023-09-23 20:49:29 +0500

    adding links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,14 @@
 import './index.scss'
-import React from 'react'
+import { useState } from 'react'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
 import {
   faHome,
   faUser,
@@ -9,44 +17,113 @@ import {
   faBars,
   faClose,
 } from '@fortawesome/free-solid-svg-icons'
-
 import { Link, NavLink } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
-const Sidbar = () => {
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
   return (
     <div className="nav-bar">
-      <Link className="logo" to="/">
-        <img src={LogoS} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      <Link className="logo" to="/" onClick={() => setShowNav(false)}>
+        {/* <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="sldan" /> */}
       </Link>
-      <nav>
-        <NavLink exact="true" activClassname="active" to="/">
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}
+        >
           <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
         </NavLink>
         <NavLink
-          exact="true"
-          activClassname="active"
+          activeclassname="active"
           className="about-link"
           to="/about"
+          onClick={() => setShowNav(false)}
         >
-          <FontAwesomeIcon
-            icon={faUser}
-            className="about-link"
-            color="#4d4d4e"
-          />
-        </NavLink>{' '}
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
         <NavLink
-          exact="true"
-          activClassname="active"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
           className="contact-link"
           to="/contact"
+          onClick={() => setShowNav(false)}
         >
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
       </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/aamir-sohail-b68001199/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/aamircheema"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="/" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="/" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faSkype}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
     </div>
   )
 }
 
-export default Sidbar
+export default Sidebar
